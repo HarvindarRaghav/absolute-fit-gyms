0d6af317365487ca6f81583c2b2bc282d3010101
unknown <harvindarraghav1@gmail.com>
2022-09-19 09:16:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './APP.CSS';
+import {Route , Routes} from 'react-router-dom';
+import {Box} from '@mui/material'
+const App = () => {
+  return (
+    <Box width="400px">
+      Navbar
+      <Routers>
+        
+      </Routers>
+    
+    
+    </Box>
+  )
+}
+
+export default App
