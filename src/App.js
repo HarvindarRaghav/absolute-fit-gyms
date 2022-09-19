b16f03ebfa7526701f60da4da4cbf07755b0a98a
unknown <harvindarraghav1@gmail.com>
2022-09-20 01:34:50 +0530

    Basic components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import './APP.CSS';
+import './App.css';
 import {Route , Routes} from 'react-router-dom';
-import {Box} from '@mui/material'
+import {Box} from '@mui/material';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+
 const App = () => {
   return (
-    <Box width="400px">
-      Navbar
-      <Routers>
+    <Box width="400px" sx={{width:{x1:'1488px'}}} m="auto">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/exercise:id' element={<ExerciseDetails/>} />
         
-      </Routers>
+      </Routes>
     
-    
+    <Footer/>
     </Box>
   )
 }
